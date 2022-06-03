61ed1d735d5d44de207839e320a860bcebc21b06
SofiaPinilla <contact@sofiapinilla.com>
2022-06-03 10:17:29 +0200

    dotenv implemented

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const { jwt_secret } = require("../config/keys.js");
+require("dotenv").config();
+const jwt_secret = process.env.JWT_SECRET
 
 const UserController = {
   async register(req, res) {
